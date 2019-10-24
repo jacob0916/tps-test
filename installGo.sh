4b43b8b69fab140ea2ef9f11510c965acb093379
Ubuntu <ubuntu@ip-172-31-14-225.us-west-1.compute.internal>
2019-10-24 09:08:41 +0000

    update installGo.sh

--- installGo.sh
+++ installGo.sh
@@ -1,9 +1,2 @@
 wget https://dl.google.com/go/go1.12.6.linux-amd64.tar.gz && sudo  tar -C /usr/local -xzf go1.12.6.linux-amd64.tar.gz
 
-vi .profile
-
-export PATH=$PATH:/usr/local/go/bin
-export GOROOT=/usr/local/go
-export GOPATH=/home/ubuntu/gopath
-
-source .profile
