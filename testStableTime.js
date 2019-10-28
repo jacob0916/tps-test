2f1c46646f9202b65f42692acc0296a5d32197f0
zhaoxiaofeng <zhao.xiaofeng@wanglutech.com>
2019-10-28 14:54:27 +0800

    add testStableTime

--- testStableTime.js
+++ testStableTime.js
@@ -1,10 +1,18 @@
+const Web3 = require('web3');
+const web3ext = require('util/web3ext.js')
+const net = require('net')
+
+
 var oldNumber = 0;
+let web3 = new Web3(new Web3.providers.IpcProvider('/home/ubuntu/data/gwan.ipc', net));
+web3ext.extend(web3)
+
 function testStableTime() {
-    var number = eth.blockNumber;
+    var number = web3.eth.blockNumber;
     if (number != oldNumber) {
-        var timeNow = eth.getBlock(number).timestamp;
-        var stableNumber = pos.getMaxStableBlkNumber();
-        var stableTime = eth.getBlock(stableNumber).timestamp;
+        var timeNow = web3.eth.getBlock(number).timestamp;
+        var stableNumber = web3.pos.getMaxStableBlkNumber();
+        var stableTime = web3.eth.getBlock(stableNumber).timestamp;
         console.log("number:", number, "stableNumber:", stableNumber, "confirmTime:", timeNow - stableTime, "timeStamp:", timeNow, stableTime);
         oldNumber = number;
     }
